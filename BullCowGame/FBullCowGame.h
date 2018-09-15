2df70cb24aa13d0990613eedd6182dc63fc2fb9c
Dargielen <dargielen@gmail.com>
2018-09-15 13:51:28 +0200

    Added comments for things to do later.

--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -10,12 +10,12 @@ public:
 	int GetMaxTries() const;
 	int GetCurrentTry() const;
 	bool isGameWon() const;
-
+	// rethink methods to use
 	int GetBulls();
 	int GetCows();
 	int GetScore();
 	bool CheckGuessValidity(std::string);
-
+	// provide a method for counting bulls and cows, and increasing try #
 private:
 	int MyCurrentTry;
 	int MyMaxTries;

a754f611f1795ab45ffffb58cb6dc82118e9e168
Dargielen <dargielen@gmail.com>
2018-09-20 16:21:02 +0200

    Added game logic; submitting guess.

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -1,33 +1,39 @@
 #include "pch.h"
 #include "FBullCowGame.h"
 
+using FString = std::string;
+using int32 = int;
+
 FBullCowGame::FBullCowGame()
 {
 	Reset();
 }
 
-int FBullCowGame::GetMaxTries() const { return MyMaxTries; }
-int FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
+int32 FBullCowGame::GetMaxTries() const { return MyMaxTries; }
+int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
 
 void FBullCowGame::Reset()
 {
-	constexpr int MAX_TRIES = 8;
+	constexpr int32 MAX_TRIES = 8;
 	MyCurrentTry = 1;
 	MyMaxTries = MAX_TRIES;
+	const FString HIDDEN_WORD = "planet";
+	MyHiddenWord = HIDDEN_WORD;
+
 	return;
 }
 
-int FBullCowGame::GetBulls()
+int32 FBullCowGame::GetBulls()
 {
 	return 0;
 }
 
-int FBullCowGame::GetCows()
+int32 FBullCowGame::GetCows()
 {
 	return 0;
 }
 
-int FBullCowGame::GetScore()
+int32 FBullCowGame::GetScore()
 {
 	return 0;
 }
@@ -37,7 +43,34 @@ bool FBullCowGame::isGameWon() const
 	return false;
 }
 
-bool FBullCowGame::CheckGuessValidity(std::string)
+bool FBullCowGame::CheckGuessValidity(FString)
 {
 	return false;
+}
+// receives a VALID guess and increments turn, and returns count
+FBullCowCount FBullCowGame::SubmitGuess(FString Guess)
+{
+	// increment the turn number
+	MyCurrentTry++;
+	// setup a return variable
+	FBullCowCount BullCowCount;
+
+	int32 HiddenWordLength = MyHiddenWord.length();
+	// loop through all letters in the guess
+	for (int32 i = 0; i < HiddenWordLength; i++) {
+		// compare letters against the hidden word
+		for (int32 j = 0; j < HiddenWordLength; j++) {
+			// if they match then
+			if (Guess[i] == MyHiddenWord[i]) {
+				// if they're in the same place
+				if (i == j) {	
+					BullCowCount.Bulls++;	// increment bulls
+				}
+				else {					
+					BullCowCount.Cows++;	// increment cows
+				}
+			}
+		}
+	}
+	return BullCowCount;
 }
--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -1,22 +1,33 @@
 #pragma once
 #include <string>
 
+using FString = std::string;
+using int32 = int;
+
+// all values initialised to zero
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
 class FBullCowGame {
 public:
 
 	FBullCowGame(); // constructor
 
 	void Reset(); //make a more rich return value
-	int GetMaxTries() const;
-	int GetCurrentTry() const;
+	int32 GetMaxTries() const;
+	int32 GetCurrentTry() const;
 	bool isGameWon() const;
 	// rethink methods to use
-	int GetBulls();
-	int GetCows();
-	int GetScore();
-	bool CheckGuessValidity(std::string);
-	// provide a method for counting bulls and cows, and increasing try #
+	int32 GetBulls();
+	int32 GetCows();
+	int32 GetScore();
+	bool CheckGuessValidity(FString);
+	FBullCowCount SubmitGuess(FString Guess);
 private:
-	int MyCurrentTry;
-	int MyMaxTries;
+	int32 MyCurrentTry;
+	int32 MyMaxTries;
+	FString MyHiddenWord;
 };
